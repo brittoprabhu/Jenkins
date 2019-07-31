b635f78c9cb4275840a08f475d98c3b25713a007
CellarstoneDevTeam <53124582+CellarstoneDevTeam@users.noreply.github.com>
2019-07-31 19:42:50 +0530

    Update BuildWeb.groovy

--- BuildWeb.groovy
+++ BuildWeb.groovy
@@ -55,7 +55,7 @@ def Patch = env.Patch
             ProjectName= ProjectName.substring(ProjectName.lastIndexOf("\\")+1)
             //Replace the project name with blank to get the full path alone
             def ProjectPath = solution.replace(ProjectName,"")
-	    ProjectPath= ProjectPath.substring(1,ProjectPath.lastIndexOf("\\")+1)
+	    ProjectPath= ProjectPath.substring(0,ProjectPath.lastIndexOf("\\")+1)
             //copy the build script file to the project path.
             def temp= solution
             def buildscriptPath=""
